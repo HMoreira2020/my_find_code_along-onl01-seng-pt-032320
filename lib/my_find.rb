0d2630dac4ca9ba3f17aeff235cc1aaa73b3f6f3
Heather Moreira <moreiraheather@gmail.com>
2020-03-27 20:59:56 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,12 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    if yield(collection[i]) 
-      return collection[i] 
+    if yield(collection[i])  #i passed to block below, and if i is divisible by 3 and 5 then..
+      return collection[i] #return collection[1]. this way it builds a new array of values that make the block return to true and this is why this method is the find method. 
     end 
     i += 1 
   end 
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
