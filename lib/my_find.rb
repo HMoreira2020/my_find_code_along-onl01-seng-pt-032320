7b4e420261180312ea5dd7238498d0e9f567c3d4
Heather Moreira <moreiraheather@gmail.com>
2020-03-27 21:01:57 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0 
   while i < collection.length 
     if yield(collection[i])  #i passed to block below, and if i is divisible by 3 and 5 then..
-      return collection[i] #return collection[1]. it returns the new array with the first value that made the block return to true. and because no code will execute after return it will stop after the first element that returns true. 
+      return collection[i] #return collection[1]. the return value of this method is the first element that made the block return to true. and because no code will execute after return it will stop after the first element that returns true. 
     end 
     i += 1 
   end 
